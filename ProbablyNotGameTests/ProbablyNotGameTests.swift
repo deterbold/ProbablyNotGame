5edcada2b699fca7e8fef34cfc1d18c02c36927e
Miguel Sicart <deterbold@gmail.com>
2024-09-27 23:44:21 +0200

    Initial Commit

---
+++ ProbablyNotGameTests/ProbablyNotGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProbablyNotGameTests.swift
+//  ProbablyNotGameTests
+//
+//  Created by Miguel Sicart on 27/09/2024.
+//
+
+import Testing
+@testable import ProbablyNotGame
+
+struct ProbablyNotGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
